292ca974dabe3aaef6541a4c80ffa9daab189b8e
meenakshidembi691 <meenakshi.dembi@dell.com>
2024-11-06 23:19:45 +0530

    Added Support for SNMP Manager

---
+++ plugins/modules/snmp_manager.py
@@ -0,0 +1,419 @@
+#!/usr/bin/python
+# Copyright: (c) 2024, Dell Technologies
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+DOCUMENTATION = r'''
+---
+
+module: snmp_manager
+version_added: '3.6.0'
+short_description: Manage SNMP Managers for PowerStore
+description:
+- Managing SNMP Managers on PowerStore Storage System includes creating
+  a SNMP Manager, getting details of SNMP Manager, modifying a
+  SNMP Manager and deleting a SNMP Manager.
+
+author:
+- Meenakshi Dembi (@dembim) <ansible.team@dell.com>
+
+extends_documentation_fragment:
+  - dellemc.powerstore.powerstore
+
+options:
+  alert_severity:
+    description:
+    - Possible severities.
+    choices: ['Info', 'Minor', 'Major', 'Critical']
+    type: str
+  auth_privacy:
+    description:
+    - Supported SNMP privacy protocol.
+    - C(None) - No encryption on the wire.
+    - C(AES256) - means Encryption class for AES 256.
+    - C(TDES) - Encryption class for Triple Data Encryption.
+    type: str
+    choices: ['None', 'AES256', 'TDES']
+  auth_protocol:
+    description:
+    - Relevant only for SNMPv3. Supported SNMP authentication protocols.
+    - C(None) - No authorization.
+    - C(MD5) - The AuthMD5 class implements the MD5 authentication protocol.
+    - C(SHA256) - The Secure Hash Authentication.
+    choices: ['None', 'MD5', 'SHA256']
+    type: str
+  ip_address:
+    description:
+    - IP address or FQDN of the SNMP server.
+    - IPv4 and IPv6 are supported.
+    type: str
+    aliases:
+      - network_name
+  new_ip_address:
+    description:
+    - IP address or FQDN of the SNMP server to update.
+    - IPv4 and IPv6 are supported.
+    type: str
+  snmp_port:
+    description:
+    - Port number to use with the address of the SNMP server.
+    type: int
+  snmp_password:
+    description:
+    - Passphrase, used for both Authentication and Privacy protocols.
+    - I(snmp_password) is only applicable when I(version) is C(V3) and to set the security level to authentication only and authentication and privacy.
+    type: str
+    aliases:
+      - auth_pass
+  snmp_username:
+    description:
+    -  User name for SNMP auth.
+    - I(snmp_username) is required when I(version) is C(V3).
+    type: str
+  trap_community:
+    description:
+    - Trap Community string describes the security level.
+    - I(trap_community) is required when I(version) is C(V2c).
+    type: str
+  update_password:
+    description:
+    - Update password applicable only to fir update case.
+    choices: ['always', 'on_create']
+    type: str
+  version:
+    description:
+    - Supported SNMP protocol versions.
+    - C(V2c) - SNMP version 2c
+    - C(V3) - SNMP version 3
+    choices: ['V3', 'V2c']
+    type: str
+  state:
+    description:
+    - Define whether the file DNS should be enabled or not.
+    - For Delete operation only, it should be set to C(absent).
+    choices: ['absent', 'present']
+    type: str
+    default: 'present'
+
+notes:
+- The I(check_mode) is supported.
+'''
+
+EXAMPLES = r'''
+
+- name: Create SNMP Manager with V2 SNMP protocol
+  dellemc.powerstore.snmp_manager:
+    array_ip: "{{ array_ip }}"
+    validate_certs: "{{ validate_certs }}"
+    user: "{{ user }}"
+    password: "{{ password }}"
+    network_name: 127.**.**.**
+    snmp_port: 49151
+    version: "V2c"
+    alert_severity: Critical
+    trap_community: test
+    state: present
+
+- name: Create SNMP Manager with V3 SNMP protocol
+  dellemc.powerstore.snmp_manager:
+    array_ip: "{{ array_ip }}"
+    validate_certs: "{{ validate_certs }}"
+    user: "{{ user }}"
+    password: "{{ password }}"
+    network_name: 127.**.**.**
+    snmp_port: 253
+    version: "V3"
+    alert_severity: Critical
+    trap_community: test
+    snmp_username: test
+    auth_protocol: MD5
+    auth_privacy: TDES
+    auth_pass: Password123!
+    state: present
+
+- name: Get SNMP Manager
+  dellemc.powerstore.snmp_manager:
+    array_ip: "{{ array_ip }}"
+    validate_certs: "{{ validate_certs }}"
+    user: "{{ user }}"
+    password: "{{ password }}"
+    network_name: 127.**.**.**
+
+- name: Modify SNMP Manager
+  dellemc.powerstore.snmp_manager:
+    array_ip: "{{ array_ip }}"
+    validate_certs: "{{ validate_certs }}"
+    user: "{{ user }}"
+    password: "{{ password }}"
+    network_name: 127.**.**.**
+    alert_severity: Info
+    trap_community: test
+    snmp_username: test
+    auth_protocol: MD5
+    auth_privacy: TDES
+    auth_pass: Password123!
+    state: present
+
+- name: Delete SNMP Manager
+  dellemc.powerstore.snmp_manager:
+    array_ip: "{{ array_ip }}"
+    validate_certs: "{{ validate_certs }}"
+    user: "{{ user }}"
+    password: "{{ password }}"
+    ip_address: 127.**.**.**
+    state: absent
+'''
+
+RETURN = r'''
+changed:
+    description: Whether or not the resource has changed.
+    returned: always
+    type: bool
+    sample: "false"
+
+snmp_details:
+    description: Details of the SNMP manager.
+    returned: When SNMP exists.
+    type: complex
+    contains:
+        id:
+            description: Unique identifier of the SNMP manager.
+            type: str
+        ip_addresses:
+            description: IPv4 address, IPv6 address, or FQDN of the SNMP manager.
+            type: str
+        port:
+            description: Port number to use with the address of the SNMP manager.
+            type: int
+        version:
+            description: Supported SNMP protocol versions.
+            type: str
+        trap_community:
+            description: Trap Community string. Usually describes the security level.
+            type: str
+        alert_severity:
+            description: Possible severities.
+            type: str
+        user_name:
+            description: User name relevant only for SNMPv3.
+            type: str
+        auth_protocol:
+            description: Relevant only for SNMPv3. Supported SNMP authentication protocols.
+            type: str
+        privacy_protocol:
+            description: Relevant only for SNMPv3. Supported SNMP privacy protocols.
+            type: str
+    sample: {
+        "id": "967ffb5d-5059-43a6-8377-1b83b99e6470",
+        "ip_address": "127.0.0.1",
+        "port": 162,
+        "version": "V3",
+        "trap_community": null,
+        "alert_severity": "Info",
+        "user_name": "admin",
+        "auth_protocol": "MD5",
+        "privacy_protocol": "AES256"
+    }
+'''
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible_collections.dellemc.powerstore.plugins.module_utils.storage.dell \
+    import utils
+from ansible_collections.dellemc.powerstore.plugins.module_utils.storage.dell.libraries.powerstore_base \
+    import PowerStoreBase
+
+
+LOG = utils.get_logger('snmp_manager')
+
+
+class PowerStoreSNMPManager(PowerStoreBase):
+    """Class with SNMP Manager Operations"""
+
+    def __init__(self):
+
+        """Define all parameters for this module."""
+
+        ansible_module_params = {
+            'argument_spec': get_powerstore_snmp_manager_parameters(),
+            'supports_check_mode': True
+        }
+        super().__init__(AnsibleModule, ansible_module_params)
+
+        self.result = dict(
+            changed=False,
+            snmp_manager_details={}
+        )
+        self.snmp_manager = self.conn.snmp_server
+
+    def get_snmp_manager(self, snmp_server_id):
+        """Get the details of NAS Server of a given Powerstore storage
+        system"""
+        return self.snmp_manager.get_snmp_server_details(snmp_server_id=snmp_server_id)
+
+    def getting_snmp_manager_id(self, ip_address):
+        """Get the id of the SNMP manager"""
+        all_snmp_managers = self.snmp_manager.get_snmp_server_list()
+        for items in all_snmp_managers:
+            if items['ip_address'] == ip_address:
+                return items['id']
+
+    def prepare_snmp_manager_create_payload(self, create_params):
+        """Prepare payload for SNMP manager creation"""
+        version = create_params['version']
+        trap_community = create_params.get('trap_community')
+        auth_protocol = create_params.get('auth_protocol')
+        privacy_protocol = create_params.get('auth_privacy')
+        payload = {
+            'port': create_params['snmp_port'],
+            'version': version,
+            'trap_community': trap_community if version != 'V3' else None,
+            'alert_severity': create_params['alert_severity'],
+            'ip_address': create_params['ip_address'],
+        }
+        if version == 'V3':
+            payload['user_name'] = create_params.get('snmp_username')
+            payload['auth_protocol'] = auth_protocol if auth_protocol else "None"
+            payload['privacy_protocol'] = privacy_protocol if privacy_protocol else "None"
+
+        if auth_protocol != "None" and version == 'V3':
+            payload['authpass'] = create_params.get('snmp_password')
+
+        return payload
+
+    def create_snmp_manager(self, create_params):
+        """Create SNMP Server"""
+        try:
+            self.validate_create(create_params)
+            msg = 'Attempting to create SNMP Server'
+            LOG.info(msg)
+            snmp_manager_details = {}
+            if not self.module.check_mode:
+                create_dict = dict()
+                create_dict = self.prepare_snmp_manager_create_payload(create_params)
+                LOG.info(create_dict)
+                resp = self.snmp_manager.create_snmp_server(payload=create_dict)
+                if resp:
+                    snmp_manager_details = self.get_snmp_manager(resp['id'])
+                msg = (f'Successfully created SNMP Server with details'
+                       f' {snmp_manager_details}')
+                LOG.info(msg)
+            return snmp_manager_details
+
+        except Exception as e:
+            msg = (f'Creation of SNMP Server on PowerStore array failed with error {str(e)} ')
+            LOG.error(msg)
+            self.module.fail_json(msg=msg, **utils.failure_codes(e))
+
+    def delete_snmp_manager(self, snmp_server_id):
+        """ Delete SNMP Manager """
+        try:
+            msg = f'Deleting SNMP Manager:' \
+                  f' {snmp_server_id}'
+            LOG.info(msg)
+            if not self.module.check_mode:
+                self.snmp_manager.delete_snmp_server(snmp_server_id=snmp_server_id)
+                return None
+            return self.get_snmp_manager(snmp_server_id)
+        except Exception as e:
+            msg = (f'Deletion of the SNMP Manager {snmp_server_id}'
+                   f' failed with error {str(e)}')
+            LOG.error(msg)
+            self.module.fail_json(msg=msg, **utils.failure_codes(e))
+
+    def validate_create(self, create_params):
+        """Perform validation of create SNMP Manager parameters"""
+        errors = []
+        if not create_params['ip_address']:
+            errors.append("Creation of SNMP Manager needs ip_address/network_name.")
+        if create_params['snmp_port']:
+            if not (162 <= create_params['snmp_port'] <= 49151):
+                errors.append("Provide a valid snmp_port value. Valid value ranges between 162-49151.")
+        if create_params['version'] == 'V2c' and not create_params['trap_community']:
+            errors.append("Trap community is required parameter for creating SNMP Manager with version V2c.")
+        if create_params['trap_community'] and len(create_params['trap_community']) > 256:
+            errors.append("The maximum length of trap community parameter is 256")
+        if create_params['version'] == 'V3' and not create_params['snmp_username']:
+            errors.append("snmp_username is required parameter for creating SNMP Manager with version V3.")
+
+        if errors:
+            self.module.fail_json(msg=', '.join(errors))
+
+
+def get_powerstore_snmp_manager_parameters():
+    """This method provides the parameters required for the ansible
+    SNMP modules on PowerStore"""
+    return dict(
+        alert_severity=dict(type='str', choices=['Info', 'Minor', 'Major', 'Critical']),
+        auth_privacy=dict(type='str', choices=['None', 'AES256', 'TDES']),
+        auth_protocol=dict(type='str', choices=['None', 'MD5', 'SHA256']),
+        ip_address=dict(type='str', aliases=['network_name']),
+        new_ip_address=dict(type='str'),
+        snmp_port=dict(type='int'),
+        snmp_password=dict(type='str', aliases=['auth_pass'], no_log=True),
+        snmp_username=dict(type='str'),
+        trap_community=dict(type='str'),
+        update_password=dict(type='str', choices=['always', 'on_create'], no_log=True),
+        version=dict(type='str', choices=['V2c', 'V3']),
+        state=dict(default='present', type='str', choices=['present', 'absent'])
+    )
+
+
+class SNMPManagerExitHandler():
+    def handle(self, snmp_obj, snmp_manager_details):
+        snmp_obj.result["snmp_manager_details"] = snmp_manager_details
+        snmp_obj.module.exit_json(**snmp_obj.result)
+
+
+class SNMPManagerDeleteHandler():
+    def handle(self, snmp_obj, snmp_manager_params, snmp_manager_details):
+        if snmp_manager_params['state'] == 'absent' and snmp_manager_details:
+            snmp_manager_details = snmp_obj.delete_snmp_manager(snmp_manager_details['id'])
+            snmp_obj.result['changed'] = True
+
+        SNMPManagerExitHandler().handle(snmp_obj, snmp_manager_details)
+
+
+class SNMPManagerModifyHandler():
+    def handle(self, snmp_obj, snmp_manager_params, snmp_manager_details):
+        # if snmp_manager_params['state'] == 'present' and snmp_manager_details:
+        #     modify_dict = snmp_obj.is_modify_required(snmp_manager_details, snmp_manager_params)
+        #     if modify_dict:
+        #         snmp_manager_details = snmp_obj.modify_snmp_manager_details()
+        #         snmp_obj.result['changed'] = True
+
+        SNMPManagerDeleteHandler().handle(snmp_obj, snmp_manager_params, snmp_manager_details)
+
+
+class SNMPManagerCreateHandler():
+    def handle(self, snmp_obj, snmp_manager_params, snmp_manager_details):
+        if snmp_manager_params['state'] == 'present' and not snmp_manager_details:
+            snmp_manager_details = snmp_obj.create_snmp_manager(create_params=snmp_manager_params)
+            snmp_obj.result['changed'] = True
+
+        SNMPManagerModifyHandler().handle(snmp_obj, snmp_manager_params, snmp_manager_details)
+
+
+class SNMPManagerHandler():
+    def handle(self, snmp_obj, snmp_manager_params):
+        snmp_manager_details = None
+        snmp_manager_id = snmp_obj.getting_snmp_manager_id(snmp_manager_params['ip_address'])
+        LOG.info("snmp_manager_id %s", snmp_manager_id)
+        if snmp_manager_id:
+            snmp_manager_details = snmp_obj.get_snmp_manager(snmp_manager_id)
+            LOG.info("snmp_manager_details %s", snmp_manager_details)
+
+        SNMPManagerCreateHandler().handle(snmp_obj, snmp_manager_params, snmp_manager_details)
+
+
+def main():
+    """ Create PowerStore SNMP object and perform action on it
+        based on user input from playbook."""
+    obj = PowerStoreSNMPManager()
+    SNMPManagerHandler().handle(obj, obj.module.params)
+
+
+if __name__ == '__main__':
+    main()
